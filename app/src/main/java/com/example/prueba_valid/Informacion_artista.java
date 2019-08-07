2f752ee243db4c46b52609940e6557f2c8c2bdcd
jairnova <31961415+jairnova@users.noreply.github.com>
2019-08-07 17:45:52 -0500

    creacion de la interfaz

---
+++ app/src/main/java/com/example/prueba_valid/Informacion_artista.java
@@ -0,0 +1,13 @@
+package com.example.prueba_valid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Informacion_artista extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_informacion_artista);
+    }
+}
